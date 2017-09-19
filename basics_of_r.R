7d90d206be20633fbeda35424a70b1ebca5f7986
tomiaJO <t.koncz@gmail.com>
2017-09-19 14:58:15 +0200

    end of class

--- basics_of_r.R
+++ basics_of_r.R
@@ -58,3 +58,19 @@ plot(z, sin(z), type = 'l')
 
 
 curve(sin, from = 0, to = 8 * pi)
+
+x <- 0
+
+#generate random numbers -1 and 1
+runif(5, min = 0, max = 1)
+
+round(runif(5, min = 0, max = 1))
+
+x <- (round(runif(100, min = 0, max = 1)) - 0.5 )* 2
+x
+cumsum(x)
+
+plot(0:99, cumsum(x), type = 's')
+
+#same thing with sample
+sample(c(1, -1), size = 100, replace = TRUE)
