12c12a695b154bf7442f82a6d28e4c96314057c7
tomiaJO <t.koncz@gmail.com>
2017-09-26 11:43:56 +0200

    -

--- CEU_DA_2.R
+++ CEU_DA_2.R
@@ -8,3 +8,37 @@ df
 df[1, 1]
 df[1,]
 df[, 1]
+
+df[1:2, ]
+df[2:3, ]
+df[c(2,3), ]
+
+str(df)
+
+df$height
+df$weight[2]
+
+
+##TODO
+
+df$bmi <- df$weight / (df$height/100)^2 ##vector operation, 3 values vs 3 values
+df$bmi
+
+summary(df$bmi)
+
+df <- read.csv('http://bit.ly/CEU-R-heights')
+df
+str(df)
+
+##convert inch -> cm
+##convert lb -> kg
+
+df$height <- df$heightIn * 2.54
+df$weight <- df$weightLb * 0.45359237
+
+df$bmi <- df$weight / (df$height/100)^2
+str(df)
+
+plot(df$height, df$weight)
+hist(df$heightIn)
+head(df)
---
+++ class_1_hw.R
@@ -0,0 +1,34 @@
+x = c(0,-1,4)
+min(x)
+
+x = runif(15, min = 0, max = 15)
+avg(x)
+
+mean(x)
+
+hist(x, main = 'Histogram of 15 random numbers')
+?hist
+
+x = seq(from = 0, to = 4 * pi, by = 0.1)
+x
+
+y = cos(x)
+plot(x,y, type='l', col='red')
+
+?plot
+
+curve(cos,from = 0, to = 4 * pi, col = 'red')
+x
+
+
+x = runif(10, min = -1, max = 1)
+mean(x)
+
+rounded 
+rounded = round(x, 2)
+rounded
+
+plot(x,rounded)
+
+x
+rounded
