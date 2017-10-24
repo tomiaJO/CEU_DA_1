b0aa38c38d95bea87cd0bfba9d69f2ee31209a95
tomiaJO <t.koncz@gmail.com>
2017-10-24 14:13:48 +0200

    initial commit

---
+++ CEU_DA_5.R
@@ -0,0 +1,14 @@
+install.packages('readxl')
+library(readxl)
+
+library(data.table)
+
+install.packages('futile.logger')
+library(futile.logger)
+flog.info('hm')
+
+begin <- 1
+v = 'begin'
+v
+(v) := 0
+?get
